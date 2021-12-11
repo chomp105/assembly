128862bafd2947c71937bb79b5ae2b6dcecd6d40
chomp105 <54012005+chomp105@users.noreply.github.com>
2021-12-11 16:46:51 -0600

    Add files via upload

--- ttt.asm
+++ ttt.asm
@@ -1,114 +1,148 @@
-;Program written by Caden Sullivan
-
-;------------------------------------
-;		Macros		    ;
-;-----------------------------------;	
-				    ;
-%macro write 2			    ;
-	mov eax, 4		    ;
-	mov ebx, 1		    ;	
-	mov ecx, %1                 ;
-	mov edx, %2                 ;
-	int 0x80                    ;
-%endmacro                           ;
-                                    ;
-%macro considerate_write 2          ;
-	push eax                    ;
-	push ebx                    ;
-	push ecx                    ;
-	push edx                    ;
-	write %1, %2                ;
-	pop edx                     ;
-	pop ecx                     ;
-	pop ebx                     ;
-	pop eax                     ;
-%endmacro                           ;
-                                    ;
-%macro read 2                       ;
-	mov eax, 3                  ;
-	mov ebx, 2                  ;
-	mov ecx, %1                 ;
-	mov edx, %2                 ;
-	int 0x80                    ;
-%endmacro			    ;
-				    ;
-%macro deref 2			    ;
-	mov eax, 3		    ;
-	mul %1			    ;
-	add eax, %2		    ;
-%endmacro                           ;
-				    ;
-;-----------------------------------;
+%macro io 4
+    mov eax, %1
+    mov ebx, %2
+    mov ecx, %3
+    mov edx, %4
+    int 0x80
+%endmacro
+%macro write 2
+    io 4, 1, %1, %2
+%endmacro
+%macro read 2
+    io 3, 2, %1, %2
+%endmacro
+%macro cwrite 2
+    push eax
+    push ebx
+    push ecx
+    push edx
+    write %1, %2
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+%endmacro
 
 section .data
 
-	board db "000000000"
-
-	newline db 0xA, 0xD
-
+    board db "000000000"
+    newline db 0xA, 0xD
+    
 section .bss
 
-	x resb 1
-	y resb 1
-	player resb 1
-
+    x resb 1
+    y resb 1
+    player resb 1
+    foo resb 1
+    
 section .text
-	global _start
+    global _start
 _start:
-
-	mov ecx, 9
-	jmp gameloop
-
+    
+    mov ecx, 9
+    jmp gameloop
+    
 gameloop:
-	
-	push ecx
-	mov ecx, 0
-	mov edx, board
-	call printboard		; print out the board and take user input
-	read x, 2
-	read y, 2
-	pop ecx
-
-	mov al, cl
-	push ecx
-	mov cl, 2
-	div cl			; get player value
-	mov cl, ah
-	xor cl, 1
-	add cl, 49 ;("0" + 1)
-	mov [player], cl
-
-	xor eax, eax
-	mov bl, [x]
-	sub bl, "0"
-	mov dl, [y]	
-	sub dl, "0"		; placing piece in specified spot
-	deref dl, ebx
-	mov [board + eax], byte cl
-
-	loop gameloop
-	jmp exit
-
+    push ecx
+    mov ecx, 0
+    mov edi, board
+    call printboard
+    read x, 2
+    read y, 2
+    pop ecx
+    mov eax, ecx
+    mov ebx, 2
+    div bl
+    xor ah, 1
+    add ah, 49
+    mov [player], ah
+    mov al, [y]
+    sub eax, "0"
+    mov ebx, 3
+    mul bl
+    add al, [x]
+    sub eax, "0"
+    mov bl, [player]
+    mov [board + eax], bl
+    push ecx
+    xor ecx, ecx
+    call checkwin
+    xor ecx, ecx
+    call checkdiag
+    pop ecx
+    dec ecx
+    jnz gameloop
+    jmp exit
+    
 printboard:
-
-	considerate_write edx, 3
-	considerate_write newline, 2
-	add edx, 3
-
+    cwrite edi, 3
+    cwrite newline, 2
+    add edi, 3
+    inc ecx
+    cmp ecx, 3
+    jl printboard
+    write newline, 2
+    ret
+    
+checkwin:
+	mov eax, 3
+	mul cl
+	mov dl, [board + eax]
+	inc eax
+	cmp dl, [board + eax]
+	jne checkloopvert
+	inc eax
+	cmp dl, [board + eax]
+	jne checkloopvert
+	cmp dl, "0"
+	je checkloopvert
+	jmp exit
+checkloopvert:
+	mov eax, ecx
+	mov dl, [board + eax]
+	add eax, 3
+	cmp dl, [board + eax]
+	jne checkloopend
+	add eax, 3
+	cmp dl, [board + eax]
+	jne checkloopend
+	cmp dl, "0"
+	je checkloopend
+	jmp exit
+checkloopend:
 	inc ecx
 	cmp ecx, 3
-	jl printboard
-	write newline, 2
+	jl checkwin
 	ret
-
-checkwin:
-
 	
-
+checkdiag:
+	mov eax, 0
+	mov dl, [board + eax]
+	add eax, 4
+	cmp dl, [board + eax]
+	jne checkotherdiag
+	add eax, 4
+	cmp dl, [board + eax]
+	jne checkotherdiag
+	cmp dl, "0"
+	je checkotherdiag
+	jmp exit
+checkotherdiag:
+	mov eax, 2
+	mov dl, [board + eax]
+	add eax, 2
+	cmp dl, [board + eax]
+	jne diagloopend
+	add eax, 2
+	cmp dl, [board + eax]
+	jne diagloopend
+	cmp dl, "0"
+	je diagloopend
+	jmp exit
+diagloopend:
 	ret
-
+	
 exit:
-	
-	mov eax, 1
-	xor ebx, ebx
-	int 0x80
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
